27909cd715d506e90f7be342b8cbb10216ee620e
Sherry <63834986+Blinqer@users.noreply.github.com>
2024-05-10 18:39:20 +0800

    Add files via upload

---
+++ Entrance to C++/Hello World.cpp
@@ -0,0 +1,20 @@
+﻿
+#include <iostream>
+using namespace std;
+
+void welcome();
+
+int main()
+{
+    cout << "=================================================" << endl;
+    cout << "             First time learning C++             " << endl;
+    cout << "=================================================\n" << endl;
+
+    std::cout << "Hello C++ World!\n";
+
+    welcome();
+
+    cin.get();
+
+}
+
---
+++ Entrance to C++/Welcome.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+void welcome()
+{
+	cout << "\nInput your name:" << endl;
+	string name; 
+	cin >> name;
+
+	cout << "\nHello, " << name << ":)" << endl;
+}
